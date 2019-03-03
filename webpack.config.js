e46bebe84626a78980f161b39e467cc77e07e4ba
Marcin Rygielski <mrogulik@gmail.com>
2019-03-04 02:49:01 +0900

    updates on webpack built

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,59 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
-module.exports = {
-    entry: './src/app.js',
-    output: {
-        path: path.join(__dirname,'/public'),
-        filename: 'bundle.js'
+module.exports = (env) => {
+    const isProduction = env === 'production'
+    const CSSExtract = new ExtractTextPlugin('styles.css')
 
-    },
-    module: {
-        rules: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: {
-                loader: 'babel-loader'
-            }
-        }, {
-            test: /\.s?css$/,
-            use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
-            ]
-        }]
-    },
-    plugins: [
-        new HtmlWebpackPlugin(
-            {
-                template: './src/index.html'
-            }
-        )
-    ],
-    
-    devServer: {
-        contentBase: path.join(__dirname,'/public'),
-        historyApiFallback: true
+        return {
+        entry: './src/app.js',
+        output: {
+            path: path.join(__dirname,'/public'),
+            filename: 'bundle.js'
+
+        },
+        module: {
+            rules: [{
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }, {
+                test: /\.s?css$/,
+                use: CSSExtract.extract({
+                    use: [
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        },
+                        {
+                            loader: 'scss-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        }
+                    ]
+                })
+            }]
+        },
+
+        plugins: [
+            new HtmlWebpackPlugin(
+                {
+                    template: './src/index.html'
+                }
+            ),
+            CSSExtract
+        ],
+        devtool: isProduction ? 'source-map' : 'inline-source-map',
+        
+        devServer: {
+            contentBase: path.join(__dirname,'/public'),
+            historyApiFallback: true
+        }
     }
 }
