0be67138575c542648429ec21ecf826af1faa7cc
Marcin Rygielski <mrogulik@gmail.com>
2019-03-05 19:54:12 +0900

    san francissco

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {NavLink} from 'react-router-dom'
-
+import moment from 'moment'
+import numeral from 'numeral'
 
 
 const ExpenseListItem = ({id, description,amount,createdAt }) => {
@@ -9,8 +10,8 @@ const ExpenseListItem = ({id, description,amount,createdAt }) => {
             <NavLink to={`/edit/${id}`}>
                 <h3>{description}</h3>
             </NavLink>
-            <p>amount: {amount}</p>
-            <p>Created at: {createdAt}</p>
+            <p>amount: {numeral(amount / 100).format('$0,0.00')}</p>
+            <p>Created at: {moment(createdAt).format('Do MMMM, YYYY')}</p>
         </div>
     )
 }
